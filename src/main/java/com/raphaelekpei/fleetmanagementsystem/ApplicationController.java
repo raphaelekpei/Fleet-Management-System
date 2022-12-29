74bd2283193d40f4c627c6f693039d5801250701
Ekpei Raphael <ekpeiraphael020@gmail.com>
2022-12-29 13:44:41 +0100

    first commit

---
+++ src/main/java/com/raphaelekpei/fleetmanagementsystem/ApplicationController.java
@@ -0,0 +1,2 @@
+package com.raphaelekpei.fleetmanagementsystem;public class ApplicationController {
+}
---
+++ src/test/java/com/raphaelekpei/fleetmanagementsystem/FleetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelekpei.fleetmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
